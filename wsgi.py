0b859b376ce251223fced73d75f3aa9989d53eb5
jzc719 <jeffreychen719@gmail.com>
2025-12-11 03:28:33 -0500

    removed wgsi

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-"""
-WSGI entrypoint for Gunicorn.
-
-Gunicorn is started with: `gunicorn wsgi:app`
-so this file *must* expose a module-level variable named `app`.
-"""
-
-from app import app  # imports the global `app` created at the bottom of app.py
